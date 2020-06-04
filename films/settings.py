2a893af0c3bb638dcdc79e7d6e0633e2edb488d4
NineMan <nelubinmv@inbox.ru>
2020-06-05 01:23:54 +0400

    Setting mysql

--- films/settings.py
+++ films/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'films.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ninemantest$filmtitles',
+        'USER': 'ninemantest',
+        'PASSWORD': 'gbPPm2K5KDhbTA^',
+        'HOST': 'ninemantest.mysql.pythonanywhere-services.com',
     }
 }
 
